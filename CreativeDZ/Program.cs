c01c9b2ebadc7498fcf3dbc9669d7a6fd425c57a
Stepan Ch <noname.churkin@gmail.com>
2023-09-21 18:07:14 +0300

    Добавьте файлы проекта.

---
+++ CreativeDZ/Program.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CreativeDZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1: Выведите на экран информацию о каждом типе данных");
+            Console.WriteLine("Тип данных | Максимальное значение | Минимальное значение");
+            Console.WriteLine($" byte | {byte.MaxValue} | {byte.MinValue}\n" +
+                $" sbyte | {sbyte.MaxValue} | {sbyte.MinValue}\n" +
+                $" short | {short.MaxValue} | {short.MinValue}\n" +
+                $" ushort | {ushort.MaxValue} | {ushort.MinValue}\n" +
+                $" int | {int.MaxValue} |  {int.MinValue}\n" +
+                $" uint | {uint.MaxValue} | {uint.MinValue}\n" +
+                $" long | {long.MaxValue} | {long.MinValue}\n" +
+                $" ulong | {ulong.MaxValue} |  {ulong.MinValue}\n" +
+                $" float | {float.MaxValue} | {float.MinValue}\n" +
+                $" double | {double.MaxValue} | {double.MinValue}\n" +
+                $" decimal | {decimal.MaxValue} | {decimal.MinValue}\n" +
+                $" char | {char.MaxValue} | {char.MinValue}");
+            Console.WriteLine("Нажмите enter");
+            Console.ReadKey();
+            Console.WriteLine("Задание 2: Напишите программу, в которой принимаются данные пользователя в виде имени,\r\nгорода, возраста и PIN-кода. Далее сохраните все значение в соответствующей\r\nпеременной, а затем распечатайте всю информацию в правильном формате. ");
+            UserInfo User1 = new UserInfo();
+            Console.WriteLine("Введите ваше имя");
+            User1.Name = Console.ReadLine();
+            Console.WriteLine("Введите ваш город:");
+            User1.City = Console.ReadLine();
+            Console.WriteLine("Введите ваш возраст:");
+            User1.Age =int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите ваш PIN-код:");
+            User1.pincode = int.Parse(Console.ReadLine());
+            Console.WriteLine($" Имя: {User1.Name}\n Город: {User1.City}\n Возраст: {User1.Age}\n PIN-код: {User1.pincode}");
+            Console.WriteLine("Нажмите enter");
+            Console.ReadKey();
+            Console.WriteLine("Задание 3: Преобразуйте входную строку: строчные буквы замените на заглавные, заглавные – настрочные.");
+            Console.WriteLine("Введите строку");
+            string input = Console.ReadLine();
+            string output = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                if (Char.IsLower(c))
+                {
+                    output += Char.ToUpper(c);
+                } else
+                {
+                    output += Char.ToLower(c);
+                }
+            }
+            Console.WriteLine("Получена строка: "+ output);
+            Console.WriteLine("Нажмите enter");
+            Console.ReadKey();
+            Console.WriteLine("Задание 4: Введите строку, введите подстроку. Необходимо найти количество вхождений и вывести на экран.");
+            Console.WriteLine("Введите строку");
+            string line= Console.ReadLine();
+            Console.WriteLine("Введите подстроку:");
+            string subline = Console.ReadLine();
+            int Count = 0;
+            for (int i=0; i< line.Length-subline.Length+1; i++)
+            {
+                if (line.Substring(i, subline.Length) == subline)
+                {
+                    Count++;
+                }
+            }
+            Console.WriteLine("Число вхождений:" + Count);
+            Console.WriteLine("Нажмите enter");
+            Console.ReadKey();
+            Console.WriteLine("Задание 5: определить, сколько бутылок виски беспошлинной торговли вам\r\nнужно будет купить, чтобы экономия по сравнению с обычной средней ценой фактически\r\nпокрыла расходы на ваш отпуск.");
+            Console.WriteLine("Введите стандартную цену виски:");
+            int normPrice = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите скидку в Duty Free");
+            int salePrice = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите стоимость отпуска");
+            int holidayPrice = int.Parse(Console.ReadLine());
+            double numberBottles = (holidayPrice) / (normPrice - salePrice);
+            Console.WriteLine("Ответ:" + Math.Floor(numberBottles));
+            Console.WriteLine("Нажмите enter");
+            Console.ReadKey();
+            Console.WriteLine("Задание 6: Воспроизвести разговор Гарри Поттера и дневника Тома Реддла.");
+            Console.WriteLine("Консоль - Привет, как к тебе обращаться?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Консоль - Привет, {name}");
+            Console.WriteLine("Пользователь - Ты знаешь что-то о тайной комнате?");
+            Console.WriteLine("Консоль - Да");
+            Console.WriteLine("Пользователь - Можешь рассказать о тайной комнате?(^人^)");
+            Console.WriteLine("Консоль - Нет");
+            System.Threading.Thread.Sleep(5000);
+            Console.WriteLine("Консоль - Но могу показать");
+            Console.BackgroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("╰(*°▽°*)╯Магия╰(*°▽°*)╯");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Нажмите enter");
+            Console.ReadKey();
+            Console.WriteLine("Задание 7: Вычислить контрольную сумму штрихкода");
+            Console.WriteLine("Введите 12 цифр");
+            int count = 0;
+            int[] digits = new int[12];
+            while (count < 12)
+            {
+                Console.WriteLine("Введите цифру: ");
+                int digit = int.Parse(Console.ReadLine());
+                if (digit < 0 || digit > 9)
+                {
+                    Console.WriteLine("Неправильная цифра");
+                    continue;
+                }
+                digits[count]= digit;
+                count++;
+            }
+            int checkdigit1 = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                if (i%2==0)
+                {
+                    checkdigit1 += digits[i];
+                } else
+                {
+                    checkdigit1 += 3 * digits[i];
+                }
+            }
+            checkdigit1 %= 10;
+            Console.WriteLine("Контрольная цифра: {0} ", checkdigit1);
+            Console.WriteLine("Используем случайные цифры: ");
+            Random random = new Random();
+            int[] digitsRand = new int[12];
+            for (int i = 0; i <12; i++)
+            {
+                digitsRand[i] = random.Next(0, 9);
+                Console.Write($" {digitsRand[i]}");
+            }
+            int checkdigit2 = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    checkdigit2 += digitsRand[i];
+                }
+                else
+                {
+                    checkdigit2 += 3 * digitsRand[i];
+                }
+            }
+            checkdigit2 %= 10;
+            Console.WriteLine("\nКонтрольная цифра: {0}", checkdigit2);
+            Console.WriteLine("Нажмите enter");
+            Console.ReadKey();
+            Console.WriteLine("Задание 8: Создать структуру студента. Посчитать общий объем алкоголя и кто сколько процентов выпил.");
+            List<Student> students = new List<Student>();
+            students.Add(new Student
+            {
+                Surname = "Иванов",
+                Name = "Иван",
+                ID = 1,
+                DateOfBirth = new DateTime(2005, 1, 1),
+                AlcoholismCategory = Student.Category.Alcoholic,
+                AlcoholVolume = 200
+            });
+            students.Add(new Student
+            {
+                Surname = "Алексин",
+                Name = "Александр",
+                ID = 2,
+                DateOfBirth = new DateTime(2006, 5, 6),
+                AlcoholismCategory = Student.Category.PartyGoer,
+                AlcoholVolume = 67
+            });
+            students.Add(new Student
+            {
+                Surname = "Попович",
+                Name = "Василий",
+                ID = 3,
+                DateOfBirth = new DateTime(2005, 3, 30),
+                AlcoholismCategory = Student.Category.Drinker,
+                AlcoholVolume = 120
+            });
+            students.Add(new Student
+            {
+                Surname = "Петров",
+                Name = "Петр",
+                ID = 4,
+                DateOfBirth = new DateTime(2004, 7, 14),
+                AlcoholismCategory = Student.Category.NonDrinker,
+                AlcoholVolume = 0
+            });
+            students.Add(new Student
+            {
+                Surname = "Сидоров",
+                Name = "Михаил",
+                ID = 5,
+                DateOfBirth = new DateTime(2005, 12, 25),
+                AlcoholismCategory = Student.Category.Alcoholic,
+                AlcoholVolume = 156
+            });
+            double totalAlcoholVolume = 0;
+            foreach (Student student in students)
+            {
+                totalAlcoholVolume += student.AlcoholVolume;
+            }
+            Console.WriteLine("Общий объем алкоголя:", + totalAlcoholVolume);
+            foreach (Student student in students)
+            {
+                double percent = (student.AlcoholVolume/totalAlcoholVolume) * 100;
+                Console.WriteLine($"{student.Surname} {student.Name} выпил {percent}% алкоголя");
+            }
+            Console.WriteLine("Нажмите enter");
+            Console.ReadKey();
+33333333333
+
+        }
+        public struct UserInfo
+        {
+            public string Name;
+            public string City;
+            public int Age;
+            public int pincode;
+        }
+        public struct Student
+        {
+            public string Surname { get; set; }
+            public string Name { get; set; }
+            public int ID { get; set; }
+            public DateTime DateOfBirth { get; set; }
+            public Category AlcoholismCategory { get; set; }
+            public double AlcoholVolume { get; set; }
+
+            public enum Category
+            {
+                Alcoholic = 'a',
+                Drinker = 'b',
+                PartyGoer = 'c',
+                NonDrinker = 'd'
+            }
+        }
+    }
+}
---
+++ Tumakov lab3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Tumakov_lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Тумаков Лабораторная работа 3 Упражнение 3.1: Создать перечислимый тип данных отображающий виды банковского\r\nсчета (текущий и сберегательный). Создать переменную типа перечисления, присвоить ей\r\nзначение и вывести это значение на печать.");
+            AccountType acctype = AccountType.Current;
+            acctype = AccountType.Saving;
+            Console.WriteLine("Тип счёта:" + acctype);
+            Console.WriteLine("Нажмите Enter");
+            Console.ReadKey();
+            Console.WriteLine("Тумаков Лабораторная работа 3 Упражнение 3.2: Создать структуру данных, которая хранит информацию о банковском\r\nсчете – его номер, тип и баланс. Создать переменную такого типа, заполнить структуру\r\nзначениями и напечатать результат.");
+            BankAccount account = new BankAccount("12345678", AccountType.Current, 9999999999);
+            Console.WriteLine("Номер счёта: {0} \nТип счёта: {1} \nБаланс счёта: {2}", account.Num, account.Type, account.Balance);
+            Console.WriteLine("Нажмите Enter");
+            Console.ReadKey();
+            Console.WriteLine("Тумаков Лабораторная работа 3 Домашнее задание 3.1: Создать перечислимый тип ВУЗ{КГУ, КАИ, КХТИ}. Создать\r\nструктуру работник с двумя полями: имя, ВУЗ. Заполнить структуру данными и распечатать.");
+            Worker worker = new Worker();
+            worker.Name = "Иванов Иван Иванович";
+            worker.University = University.KGU;
+            Console.WriteLine("Имя паботника: {0}\n ВУЗ работника {1}", worker.Name, worker.University);
+            Console.WriteLine("Нажмите Enter");
+            Console.ReadKey();
+
+
+
+
+        }
+        enum University
+        {
+            KGU,
+            KAI,
+            KHT
+        }
+        struct Worker
+        {
+            public string Name { get; set; }
+            public University University { get; set; }
+        }
+        enum AccountType
+        {
+            Current,
+            Saving
+        }
+        struct BankAccount
+        {
+            public string Num { get; set; }
+            public AccountType Type { get; set; }
+            public decimal Balance { get; set; }
+            public BankAccount(string num, AccountType type, decimal balance)
+            {
+                Num = num;
+                Type = type;
+                Balance = balance;
+            }
+
+        }
+    }
+}
+
